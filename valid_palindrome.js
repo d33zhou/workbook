3de3ac97be145f8d392f9e75e30df3dd548b8760
David Zhou <d33zhou@outlook.com>
2022-03-16 23:59:18 -0400

    add valid palindrome

---
+++ valid_palindrome.js
@@ -0,0 +1,8 @@
+const isPalindrome = function (s) {
+  let converted = "";
+
+  for (let i = 0; i < s.length; i++) {}
+};
+
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("bobsled")); // false
