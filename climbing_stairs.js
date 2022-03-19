38ef2a1854c97b329aab92097dd01bc5af1ee8eb
David Zhou <d33zhou@outlook.com>
2022-03-18 23:58:59 -0400

    add climbing stairs

---
+++ climbing_stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {
+  let counter = 0;
+
+  if (n <= 1) {
+    return n;
+  } else {
+    //recursive
+  }
+};
+
+console.log(climbStairs(2)); // 2
+console.log(climbStairs(3)); // 3
