7252857468e0f0db7725a033318b6b888f394b22
David Zhou <d33zhou@outlook.com>
2022-03-28 23:59:23 -0400

    update buy sell stocks

--- buy_sell_stocks.js
+++ buy_sell_stocks.js
@@ -7,15 +7,33 @@ const maxProfit = function (prices) {
   // can loop through each array element, and take the difference
   // only return positive values
 
-  let max = 0;
+  // find the largest and smallest numbers, and check if smallest is before largest
+  // if YES, then return that as the max profit
+  // if NO, then check largest against second smallest, AND smallest against second largest
+  // if both are valid (i.e. smallest is before largest), then return the higher of the profits
+  // if only one is valid, return that as the max profit
+  // if neither is valid, then check the third set etc.
 
-  for (let b = 0; b < prices.length; b++) {
-    for (let s = 0; s < prices.length; s++) {
-      if (s > b && prices[s] - prices[b] > max) {
-        max = prices[s] - prices[b];
-      }
-    }
+  // keep in mind, only need to return the highest profit, NOT the index or actual amounts << key point
+
+  // for (let i = 0; i < prices.length; i++) {}
+
+  // for (let b = 0; b < prices.length; b++) {
+  //   for (let s = 0; s < prices.length; s++) {
+  //     if (s > b && prices[s] - prices[b] > max) {
+  //       max = prices[s] - prices[b];
+  //     }
+  //   }
+  // }
+
+  let profit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    let max = Math.max(...prices);
+    let min = Math.min(...prices);
   }
 
-  return max;
+  return profit;
 };
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
