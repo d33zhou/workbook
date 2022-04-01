a4fd207fac14fcc1adc0cd8353ccaa7345e595fe
David Zhou <d33zhou@outlook.com>
2022-04-01 00:05:28 -0400

    complete destination city

--- destination_city.js
+++ destination_city.js
@@ -3,11 +3,11 @@
  * @return {string}
  */
 const destCity = function (paths) {
-  // loop through array of arrays (path)
-  // put each starter city into an array
-  // loop through arrays again
-  // remove each ending city from the array
-  // last item is the destination
+  // loop through array of arrays --> paths
+  // put each ending city destination into a new array
+  // loop through full paths array again
+  // remove each ending city from the new array if there is a corresponding starting city
+  // last city left in the new array is the final destination
 
   const cities = [];
 
@@ -15,7 +15,21 @@ const destCity = function (paths) {
     cities.push(paths[i][1]);
   }
 
-  for (let j = 0; j < paths.length; j++) {}
+  for (let j = 0; j < paths.length; j++) {
+    if (cities.includes(paths[j][0])) {
+      const toRemoveCity = cities.indexOf(paths[j][0]);
+      cities.splice(toRemoveCity, 1);
+      j--;
+    }
+  }
 
   return cities[0];
 };
+
+console.log(
+  destCity([
+    ["London", "New York"],
+    ["New York", "Lima"],
+    ["Lima", "Sao Paulo"],
+  ])
+); // Sao Paulo
