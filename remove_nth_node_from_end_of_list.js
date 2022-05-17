f60fa067fd4cef47ca63535b60a3f3d3e06b8357
David Zhou <d33zhou@outlook.com>
2022-05-17 14:24:38 -0400

    refactor return nth node from end of list

--- remove_nth_node_from_end_of_list.js
+++ remove_nth_node_from_end_of_list.js
@@ -10,28 +10,53 @@
  * @param {number} n
  * @return {ListNode}
  */
+// const removeNthFromEnd = function (head, n) {
+//   let pointer = head;
+//   let listLength = 0;
+
+//   while (pointer) {
+//     pointer = pointer.next;
+//     listLength++;
+//   }
+
+//   let nodeBeforeRemovedN = listLength - n - 1;
+
+//   if (listLength === n) {
+//     return head.next;
+//   }
+
+//   pointer = head;
+
+//   for (let i = 0; i < nodeBeforeRemovedN; i++) {
+//     pointer = pointer.next;
+//   }
+
+//   pointer.next = pointer.next.next;
+
+//   return head;
+// };
+
 const removeNthFromEnd = function (head, n) {
   let pointer = head;
-  let listLength = 0;
+
+  for (let i = 0; i < n; i++) {
+    pointer = pointer.next;
+  }
+
+  if (!pointer) {
+    return head.next;
+  } else {
+    pointer = pointer.next;
+  }
+
+  let pointerN = head;
 
   while (pointer) {
     pointer = pointer.next;
-    listLength++;
+    pointerN = pointerN.next;
   }
 
-  let nodeBeforeRemovedN = listLength - n - 1;
-
-  if (listLength === n) {
-    return head.next;
-  }
-
-  pointer = head;
-
-  for (let i = 0; i < nodeBeforeRemovedN; i++) {
-    pointer = pointer.next;
-  }
-
-  pointer.next = pointer.next.next;
+  pointerN.next = pointerN.next.next;
 
   return head;
 };
